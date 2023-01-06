c491fe799b0db1c42f366ed07969f38564518e2e
Michał S <szplit.michal@gmail.com>
2023-01-06 17:11:03 +0100

    Add dictionary alterations loops

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ lista = {
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+sklepy = []
+produkty = []
+
+for keys in lista.keys():
+  sklepy.append(keys.title())
+
+for keys, values in lista.items():
+  produkty.append(values)
